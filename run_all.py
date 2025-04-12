9ea6e3847019bf4483c7536edee7ccd92985a292
Jose Emilio Labra Gayo <jelabra@gmail.com>
2025-04-12 16:11:13 +0200

    Adding possibility to store results

--- run_all.py
+++ run_all.py
@@ -1,40 +1,101 @@
 #!/usr/bin/python
 import os
 import argparse
+import re
 import yaml
 import rdflib
 
 shacl_prefix = "http://www.w3.org/ns/shacl#"
 
-def shacl_tq(filename, name, results_folder, config):
+def shacl_tq(filename, name, results_folder, config, results):
     technology_name = "shacl_tq"
-    command = f"shaclvalidate.sh -datafile {filename} > {results_folder}/{name}_{technology_name}.results"
+    temp = config['temp']
+    validation_report_file_temp = os.path.join(temp, f"{name}_{technology_name}_results_temp.ttl")
+    command = f"shaclvalidate.sh -datafile {filename} > {validation_report_file_temp}"
     if config['verbose'] > 0 :
         print(f"Running: {command}")
     os.system(command)
+    validation_report_file = os.path.join(results_folder, f"{name}_{technology_name}_results_temp.ttl")
+    validation_output = os.path.join(results_folder, f"{name}_{technology_name}_output.txt")
+    regex = re.compile('.*Failure.*')
+    split_file_by_regex(validation_report_file_temp, regex, validation_output, validation_report_file)
+    result = analyze_validation_report(validation_report_file)
+    store_result(name, technology_name, result, results)
 
-def shacl_jena(filename, name, results_folder, config):
+def shacl_jena(filename, name, results_folder, config, results):
     technology_name = "shacl_jena"
-    # Construct the command to run the test
-    command = f"shacl v {filename} > {results_folder}/{name}_{technology_name}.results"
+    validation_report_file = os.path.join(results_folder, f"{name}_{technology_name}_results.ttl")
+    # validation_output = os.path.join(results_folder, f"{name}_{technology_name}_output.txt")
+    command = f"shacl v {filename} > {validation_report_file}"
     if config['verbose'] > 0 :
         print(f"Running: {command}")
     os.system(command)
+    result = analyze_validation_report(validation_report_file)
+    store_result(name, technology_name, result, results)
 
-def shaclex(filename, name, results_folder, config):
+def shaclex(filename, name, results_folder, config, results):
     technology_name = "shaclex"
-    command = f"shaclex --validate --engine SHACLEX --data {filename} --validationReportFormat TURTLE --showValidationReport > {results_folder}/{name}_{technology_name}.results"
+    validation_report_file = os.path.join(results_folder, f"{name}_{technology_name}_results.ttl")
+    validation_output = os.path.join(results_folder, f"{name}_{technology_name}_output.txt")
+    command = f"shaclex --validate --engine SHACLEX --data {filename} --validationReportFormat TURTLE --showValidationReport --validationReportFile {validation_report_file} > {validation_output}"
     if config['verbose'] > 0 :
         print(f"Running: {command}")
     os.system(command)
+    result = analyze_validation_report(validation_report_file)
+    store_result(name, technology_name, result, results)
 
-def pyshacl(filename, name, results_folder, config):
+def pyshacl(filename, name, results_folder, config, results):
     technology_name = "pyshacl"
-    command = f"pyshacl {filename} > {results_folder}/{name}_{technology_name}.results"
+    validation_report_file = os.path.join(results_folder, f"{name}_{technology_name}_results.ttl")
+    validation_output = os.path.join(results_folder, f"{name}_{technology_name}_output.txt")
+    command = f"pyshacl {filename} -o {validation_report_file} --format turtle > {validation_output}"
     if config['verbose'] > 0 :
         print(f"Running: {command}")
     os.system(command)
+    result = analyze_validation_report(validation_report_file)
+    print(f"Result: {result}")
+    store_result(name, technology_name, result, results)
 
+def analyze_validation_report(filename):
+    # Load the validation report
+    g = rdflib.Graph()
+    g.parse(filename, format='turtle')
+
+    # Extract the number of violations
+    query = """
+        SELECT ?conforms
+        WHERE {
+            ?s a sh:ValidationResult .
+            ?s sh:conforms ?conforms .
+        }
+    """
+    result = g.query(query)
+    for row in result:
+        conforms = row[0]
+        if conforms == rdflib.Literal("true"):
+            print("The data graph conforms to the SHACL shapes.")
+        else:
+            print("The data graph does not conform to the SHACL shapes.")
+    return result
+
+
+# Split the file into two files based on a regex pattern
+def split_file_by_regex(source, regex, file1, file2):
+    with open(source, 'r') as infile, open(file1, 'w') as outfile1, open(file2, 'w') as outfile2:
+        for line in infile:
+            if regex.match(line):
+                outfile1.write(line)
+            else:
+                outfile2.write(line)
+
+def store_result(name, technology_name, result, results):
+    results.append({
+        "name": name,
+        "technology_name": technology_name,
+        "result": result
+    }) 
+    
+    
 parser = argparse.ArgumentParser(description="Execute Recursion Shapes experiments",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-v", "--verbose", help="increase verbosity", default = 0, action="count")
@@ -90,12 +151,16 @@ for test in manifest['tests']:
 
     print(f"Merged graph: {g.serialize(format='turtle')}")
     # Save the merged graph to a file
-    merged_filename = os.path.join(temp, filename)
+    merged_filename = os.path.join(temp, name + ".ttl")
     g.serialize(destination=merged_filename, format='turtle')
+
     print(f"Serialized graph to {merged_filename}")
-    
+    results = []
     if engine == "shacl":
-        shacl_tq(merged_filename, name, results_folder, config)
-        shacl_jena(merged_filename, name, results_folder,config)
-        shaclex(merged_filename, name, results_folder,config)
-        pyshacl(merged_filename, name, results_folder,config)
+        shacl_tq(merged_filename, name, results_folder, config, results)
+        shacl_jena(merged_filename, name, results_folder,config, results)
+        shaclex(merged_filename, name, results_folder,config, results)
+        pyshacl(merged_filename, name, results_folder,config, results)
+    print(f"Results: {results}")
+
+
